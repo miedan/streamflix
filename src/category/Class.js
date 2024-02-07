00e035af1cf70e6eb4fe4b7a0ceb281d85636014
miedan <miedanbezuayehu@gmail.com>
2024-02-07 16:25:55 +0300

    updating the movie row

--- src/category/Class.js
+++ src/category/Class.js
@@ -1,86 +1,61 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
-const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    arrows: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-    ],
+const Row = ({ title, fetchurl }) => {
+  const [movies, setMovies] = useState([]);
+  const [currentindex, setcurrentindex] = useState(0)
+
+  const slideleft = () => {
+
+    setcurrentindex(currentindex - 1)
+  }
+
+  const slideright = () => {
+    setcurrentindex(currentindex + 1)
     
-  };
+  }
 
-const Row = ({title, fetchurl}) => {
-
-  const [movies, setmovies] = useState([])
-  const [next, setnext] = useState(false)
-  
 
   useEffect(() => {
-    const fetchmovie = async() => {
-       
-        const response = await fetch(fetchurl);
-        const data = await response.json();
-        setmovies(data.results)
-        console.log(data)
-    }
-    fetchmovie()
-   
-  }, [])
+    const fetchMovies = async () => {
+      const response = await fetch(fetchurl);
+      const data = await response.json();
+      setMovies(data.results);
+    };
+    fetchMovies();
+  }, [fetchurl]);
 
-
-    
   return (
-     
     <div>
-          <h1 className='flex justify-start'>{title}</h1>
-          
-      
-      <Slider {...settings}>
-      {/* <div className='flex '> */}
-        {movies.map((movie) => (
-       <div className='pr-3 pb-8'  key={movie.id} >
-         <img className='' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt='movie photo'/>
-       </div>
-      ))}
-       {/* </div> */}
+      <h1 className='flex justify-start'>{title}</h1>
 
-      </Slider>
-      
-       
-  
+      <div className='flex relative'>
+        
 
+        
+        
+
+        {movies.slice(currentindex * 5, (currentindex+1) * 5 ).map((movie) => (
+          <div className='pr-3 pb-8' key={movie.id}>
+            <img
+              className=''
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt='movie photo'
+            />
+          </div>
+        ))}
+
+<div className='absolute inset-y-0 left-0 flex items-center'>
+        {currentindex > 0 && <FaChevronLeft onClick={slideleft} />}
+        </div>
+        <div className='absolute inset-y-0 right-0 flex items-center'>
+        {currentindex < Math.ceil(movies.length / 5) - 1  &&<FaChevronRight onClick={slideright} />}
+        </div>
+
+        
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Row
+export default Row;
